5d35e43671802325267c4f53005fb99b9dce03a1
BehzadBz <43294940+BehzadBz@users.noreply.github.com>
2025-03-04 16:15:29 +0330

    Fixed error where deleting a booking leads to an internal error.

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -90,13 +90,18 @@ export async function deleteBooking(bookingId: string) {
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingIds = guestBookings.map((booking) => booking.id);
 
-  if (!guestBookingIds.includes(bookingId))
-    throw new Error("You are not" + " allowed to delete this booking!");
+  console.log("Booking ID to delete:", bookingId);
+  console.log("Guest Booking IDs:", guestBookingIds);
+
+  const bookingIdNumber = Number(bookingId);
+
+  if (!guestBookingIds.includes(bookingIdNumber))
+    throw new Error("You are not allowed to delete this booking!");
 
   const { error } = await supabase
     .from("bookings")
     .delete()
-    .eq("id", bookingId);
+    .eq("id", bookingIdNumber);
 
   if (error) throw new Error("Booking could not be deleted");
 
